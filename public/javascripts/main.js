2771021eddb2480416b1bde6e305e8c80ee2ab56
Jeffery Bennett <meandmybadself@gmail.com>
2016-09-29 22:08:49 -0500

    Updates.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -224,6 +224,9 @@ function updateTable (id, count) {
 function createTweet (t) {
   var li = $("<li class='tweet'><dl><dt><a href='https://twitter.com/" + t.screenname + "' target='_blank'>" + t.screenname + "</a></dt><dd><div><a href='https://twitter.com/statuses/" + t.id + "' target='_blank'>" + t.text + '</a></div><div><img class="cereal" src="/assets/images/textures/cereals/' + t.cereal + '.jpg" alt="' + t.cereal + '"/></div></dd></dl></li>')
   $('#tweets').append(li)
+  if ($('#tweets').children().length > 4) {
+    $('#tweets .tweet:last-child').remove();
+  }
 }
 
 function connectToSocket () {
